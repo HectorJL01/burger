3eaced3fa8d5b4b18b3e4d8cd58de3a907eb2490
Héctor Lopez <hectorjl01@Hectors-MacBook-Pro.local>
2018-05-10 18:37:43 -0400

    rev

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,17 +1,17 @@
-// Pull in required dependencies
+//  dependencies
 var express = require('express');
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
-var burger = require('../models/burger.js');
+// Import the model (burger.js) for data
+var burger = require('../models/burgers.js');
 
-// Create the routes and associated logic
+// routes 
 router.get('/', function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
-    // console.log(hbsObject);
+  
     res.render('index', hbsObject);
   });
 });
